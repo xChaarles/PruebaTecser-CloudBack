64ba3d9c6c79eb3ae56635b2d025bedc37bfd2e3
xchaarles <carlosbp2608@gmail.com>
2024-12-13 19:30:14 -0500

    Creacion de proyecto y entidad User

---
+++ src/main/java/com/PruebaTecser/Cloud/Entidades/User.java
@@ -0,0 +1,20 @@
+package com.PruebaTecser.Cloud.Entidades;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String street;
+    private Date fecha;
+
+}
